793ff9b4dffea848276298936fc109c6243e5768
Andy Zheng <97316979+Andrew-bro2022@users.noreply.github.com>
2023-02-02 23:25:37 -0500

    Update core.h

--- core.h
+++ core.h
@@ -1,15 +1,7 @@
 /*
 *****************************************************************************
-                        Assignment 1 - Milestone 3
+File: core.h
 Full Name  : Yuchi Zheng
-Student ID#: 025 848 151
-Email      : yzheng103@myseneca.ca
-Section    : NAA
-
-Authenticity Declaration:
-I declare this submission is the result of my own work and has not been
-shared with any other student or 3rd party content provider. This submitted
-piece of work is entirely of my own creation.
 *****************************************************************************
 */
 
